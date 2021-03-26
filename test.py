9bf7b692484d6f3c5e14d65e798c52478c00e00e
Maxim Rastorguev <rastorguev10@gmail.com>
2021-03-26 10:48:08 +0300

    test_autorize

--- test.py
+++ test.py
@@ -1,9 +1,28 @@
-from flask import Flask
-app = Flask(__name__)
+logins_passwords = {"Progoger": "ProForAll123", "Petushok": "GachaForLife123", "LukNi": "LukIamYourFather123"}
 
-@app.route('/')
-def hello_world():
-    return 'Hello'
+import flask
+
+app = flask(__name__)
+
+@app.route('/test', methods=["POST"])
+def autorize():
+    login = flask.request.form.get("login")
+    password = flask.request.form.get("password")
+    res = flask.request.get("https://mystudyksu.herokuapp.com/")
+
+    if res.status_code != 200:
+        return flask.jsonify({"success": False})
+
+    data = res.json()
+
+    if data['login'] in logins_passwords.keys():
+        if logins_passwords[login] == data['password']:
+            return flask.jsonify({"success": True})
+        else:
+            return flask.jsonify({"success": False})
+    else:
+        return flask.jsonify({"success": False})
+
 
 if __name__ == '__main__':
     app.run()
