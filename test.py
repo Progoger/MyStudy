f9dcb9b8b43309a4bbdc0df369c89653dfc21136
Maxim Rastorguev <rastorguev10@gmail.com>
2021-03-26 11:24:46 +0300

    test_with_front

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
-logins_passwords = {"Progoger": "ProForAll123", "Petushok": "GachaForLife123", "LukNi": "LukIamYourFather123"}
-
 import flask
+import uuid
+
+
+logins_passwords = {"Progoger": "ProForAll123", "Petushok": "GachaForLife123", "LukNi": "LukIamYourFather123"}
+uuid_data = {"2sdfasd-asdgfdfasg-d-asgsadg":{"organisation": 'Костромской Государственный Университет', "color": '1b37d8'}}
+
 
 app = flask(__name__)
 
-@app.route('/test', methods=["POST"])
+@app.route('/autorize', methods=["POST"])
 def autorize():
     login = flask.request.form.get("login")
     password = flask.request.form.get("password")
@@ -16,12 +20,29 @@ def autorize():
     data = res.json()
 
     if data['login'] in logins_passwords.keys():
-        if logins_passwords[login] == data['password']:
-            return flask.jsonify({"success": True})
+        if logins_passwords[password] == data['password']:
+            autorize_uuid = '2sdfasd-asdgfdfasg-d-asgsadg'#uuid.uuid4()
+            uuid_login[autorize_uuid] = login
+            return flask.jsonify({"success": True, "uuid":autorize_uuid})
         else:
             return flask.jsonify({"success": False})
     else:
         return flask.jsonify({"success": False})
+
+@app.route('/check_uuid', methods=["POST"])
+def check_uuid():
+    uuid = flask.request.form.get("uuid")
+    if uuid == '2sdfasd-asdgfdfasg-d-asgsadg':
+        return True
+    else:
+        return False
+
+
+@app.route('/send_data', methods=["POST"])
+def send_data():
+    uuid = flask.request.form.get("uuid")
+    if uuid in uuid_data.keys():
+        return flask.jsonify(uuid_data[uuid])
 
 
 if __name__ == '__main__':
