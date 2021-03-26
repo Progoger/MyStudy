a02aacba0e9f16bb3f50e8076b17d7d6c81a3ac9
nl.lukyanov <nl.lukyanov@tensor.ru>
2021-03-26 20:48:54 +0300

    Прикрутили интерфейс

---
+++ generals/database.py
@@ -0,0 +1,28 @@
+import psycopg2 as pg
+
+conn = pg.connect(
+    user='tatfezxgvgaefs',
+    password='05deb9321d5f3b4420fdccdd507e21f29c06baf2b8cd3d080ba39ef42e374d5e',
+    host='ec2-54-155-208-5.eu-west-1.compute.amazonaws.com',
+    port=5432,
+    database='dm6a2f8rs68e1'
+)
+
+
+def SqlQuery(request, *params, my_conn=None):
+    """
+    Выполняет sql запрос
+    :param request: Запрос. Для корректной вставки в запрос параметров нужно в запросе вставлять %s
+    Пример: "select * from "table" where "type" = %s"
+    :param params: Параметры, которые нужно вставить запрос
+    :param my_conn: Потом расскажу
+    :return:
+    """
+    if my_conn is None:
+        cur = conn.cursor()
+    else:
+        cur = my_conn.cursor()
+    cur.execute(request, tuple(params))
+    if my_conn is None:
+        conn.commit()
+    return cur.fetchall()
--- test.py
+++ test.py
@@ -1,56 +1,65 @@
-import flask
+from flask import Flask, request, jsonify, send_from_directory
+# from generals.database import SqlQuery
+# from werkzeug.security import generate_password_hash, check_password_hash
+
+# print(str(generate_password_hash('test')))
+# print(check_password_hash('pbkdf2:sha256:150000$fFDjnPFl$cbc2f1add72702ad9f7c7f2582e3acb978269da637467798fa3e63707c7f2125', 'test'))
 
 
 logins_passwords = {"Progoger": "ProForAll123", "Petushok": "GachaForLife123", "LukNi": "LukIamYourFather123"}
 uuid_data = {"2sdfasd-asdgfdfasg-d-asgsadg":{"organisation": 'Kostroma State University', "color": '1b37d8'}}
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
+
+
+@app.route('/<path:path>', methods=['GET'])
+def static_proxy(path):
+  return send_from_directory('./static', path)
 
 @app.route('/')
 def hello_world():
-    return 'Hello'
+    return send_from_directory('./templates', 'index.html') # render_template('index.html')
 
 
 @app.route('/autorize', methods=["POST"])
 def autorize():
-    params = flask.request.get_json(force=True)
+    params = request.get_json(force=True)
     if not params:
-        return flask.jsonify({"success": False})
+        return jsonify({"success": False})
     login = params['login']
     password = params['password']
     if login in logins_passwords.keys():
         if logins_passwords[login] == password:
             autorize_uuid = '2sdfasd-asdgfdfasg-d-asgsadg'#uuid.uuid4()
-            return flask.jsonify({"success": True, "uuid": autorize_uuid})
+            return jsonify({"success": True, "uuid": autorize_uuid})
         else:
-            return flask.jsonify({"success": False})
+            return jsonify({"success": False})
     else:
-        return flask.jsonify({"success": False})
+        return jsonify({"success": False})
 
 
 @app.route('/check_uuid', methods=["POST"])
 def check_uuid():
-    if flask.request.get_json(force=True):
-        uuid = flask.request.get_json(force=True)['uuid']
+    if request.get_json(force=True):
+        uuid = request.get_json(force=True)['uuid']
         if uuid == '2sdfasd-asdgfdfasg-d-asgsadg':
-            return flask.jsonify({"success": True})
+            return jsonify({"success": True})
         else:
-            return flask.jsonify({"success": False})
+            return jsonify({"success": False})
     else:
-        return flask.jsonify({"success": False})
+        return jsonify({"success": False})
 
 
 @app.route('/send_data', methods=["POST"])
 def send_data():
-    if flask.request.get_json(force=True):
-        uuid = flask.request.get_json(force=True)['uuid']
+    if request.get_json(force=True):
+        uuid = request.get_json(force=True)['uuid']
         if uuid in uuid_data.keys():
-            return flask.jsonify({"success": True, "result": uuid_data[uuid]})
+            return jsonify({"success": True, "result": uuid_data[uuid]})
         else:
-            return flask.jsonify({"success": False})
+            return jsonify({"success": False})
     else:
-        return flask.jsonify({"success": False})
-
+        return jsonify({"success": False})
 
 
 if __name__ == '__main__':
