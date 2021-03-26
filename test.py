2966e8ca3572f07e54c69bb23b81e54c3841f6db
Maxim Rastorguev <rastorguev10@gmail.com>
2021-03-26 15:00:24 +0300

    test_front

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ def hello_world():
 @app.route('/check', methods=["POST"])
 def check():
     if flask.request.get_json(force=True):
-        return flask.request.get_json(force=True)
+        return flask.request.get_json(force=True)['login']
 
 @app.route('/autorize', methods=["POST"])
 def autorize(params):
